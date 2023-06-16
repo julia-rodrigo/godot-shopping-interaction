ca072265d1db2a41621f96057f4ca5f57a970ac0
julia-rodrigo <juliarodrigo2301@gmail.com>
2023-06-16 22:06:23 +0100

    commit 1

---
+++ READ_ME.gd
@@ -0,0 +1,20 @@
+# READ ME FIRST
+
+#Open this project in Godot v3. It wont work on godot v4
+#You are only allowed to open "TinyWorld.tscn" or else the code breaks.
+#This project is a smaller version of "Godot Theme Park".
+#
+#This project demonstrates a 3rd person version instead 
+#of first person perspective (for those with no VR headset)
+#
+#The reason you are to open "TinyWorld.tscn", is because of 
+#"Global.gd". The current_main_scene var sets the scene you could open
+#
+#The scene initialises the Player and the variables for saving your items.
+#
+# when you open "TinyWorld.tscn", please give some time to load. 
+#
+# Talking with the shop keepers will lag until a few seconds until the game starts
+# interact with the shop keepers using "w" and press the buttons to buy items
+# Open you inverntory and you can hit 'p' to take a picture, 
+# but it will also require a few seconds for completion
---
+++ WORLD_SCENE/Main/From_Ruby/NPC/NPCInteractions/ConversationArea.gd
@@ -0,0 +1,88 @@
+extends Area
+
+onready var dialogue_box = preload("res://WORLD_SCENE/Main/From_Ruby/NPC/NPCInteractions/NPCDialogues/DialogueBox.tscn").instance()
+onready var character_name_label = dialogue_box.get_node("outer/Panel/inner/list/name")
+
+onready var shop_area = preload("res://WORLD_SCENE/Main/Shop/ShopArea.tscn").instance()
+
+onready var talk_panel = $Control/MarginContainer/center/TalkToMe
+onready var control_node = $Control
+
+var character_name = "NO CHARACTER NAME!"
+var message = [ "NO MESSEAGES EITHER", "oops", "I WONDER WHY"]
+var talk_to_me = "interact"
+
+var can_interact = false
+var player_body 
+
+var shopkeeper
+var items_to_buy = []
+var shop_name
+
+
+func _ready():
+	talk_panel.get_node("inner/separate/press").text = talk_to_me
+	talk_panel.hide()
+
+func _on_ConversationArea_body_entered(body):
+	if body.name == 'Ruby':
+		player_body = body 
+		talk_panel.show()
+		can_interact = true
+
+func _on_ConversationArea_body_exited(body):
+	if body.name == 'Ruby':
+		talk_panel.hide()
+		can_interact = false
+
+func _input(_event):
+	
+	if Input.is_key_pressed(KEY_W) and can_interact and Player.move:
+		
+		if shopkeeper and items_to_buy.size() == 0:
+			dialogue_box.dialogue = ['Thanks to you, I got everything sold!', 'Really appreciate it man xD']
+			dialogue_box.name_of_character = character_name
+			add_child(dialogue_box)
+			dialogue_box = preload("res://WORLD_SCENE/Main/From_Ruby/NPC/NPCInteractions/NPCDialogues/DialogueBox.tscn").instance()
+			return
+			
+		dialogue_box.dialogue = message
+		dialogue_box.name_of_character = character_name
+		add_child(dialogue_box)
+		
+		dialogue_box.connect("dialogue_ended", self, "_finished_conversation")
+		dialogue_box = preload("res://WORLD_SCENE/Main/From_Ruby/NPC/NPCInteractions/NPCDialogues/DialogueBox.tscn").instance()
+		
+		
+func _finished_shopping ():
+	print("\nShopping with " + character_name + " finished")
+	
+	dialogue_box.dialogue = [ "See you again :D"]
+	dialogue_box.name_of_character = character_name
+	
+	items_to_buy = Global.shopkeeper_updated_merchandise
+	
+	add_child(dialogue_box)
+	
+	dialogue_box.connect("dialogue_ended", self, "move_again")
+	dialogue_box = preload("res://WORLD_SCENE/Main/From_Ruby/NPC/NPCInteractions/NPCDialogues/DialogueBox.tscn").instance()
+
+func move_again():
+	Player.move = true
+
+func _finished_conversation ():
+	print("\nconversation with " + character_name + " finished")
+	if shopkeeper:
+		Player.move = false
+		Global.shopkeeper_updated_merchandise = items_to_buy
+		
+		shop_area.dialogue = message
+		shop_area.merchandise = items_to_buy
+		shop_area.shopkeeper_name = character_name
+		shop_area.shop_name = shop_name
+		
+		add_child(shop_area)
+		shop_area.connect("shopping_ended", self, "_finished_shopping")
+		shop_area = preload("res://WORLD_SCENE/Main/Shop/ShopArea.tscn").instance()
+		
+		
